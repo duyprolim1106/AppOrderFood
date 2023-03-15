ea811f9603b4e64e3facd2c0df9879930fffc7b1
nhkd1106 <duynguyen11062002@gmail.com>
2023-03-15 09:50:10 +0700

    commit

--- app/src/main/java/com/example/apporderfood/HomeFragmentActivity.java
+++ app/src/main/java/com/example/apporderfood/HomeFragmentActivity.java
@@ -2,7 +2,11 @@ package com.example.apporderfood;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.WindowInsets;
+import android.view.WindowManager;
 
 public class HomeFragmentActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class HomeFragmentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_fragment);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            getWindow().setDecorFitsSystemWindows(false);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+            getWindow().setNavigationBarColor(Color.TRANSPARENT);
+            getWindow().getInsetsController().hide(WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
+        } else {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }
+
     }
 }
