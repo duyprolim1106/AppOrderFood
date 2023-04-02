22b9f828208470fb4b8b66a690c6c7bb70fe8971
nhkd1106 <duynguyen11062002@gmail.com>
2023-04-02 19:03:06 +0700

    commit

--- app/src/main/java/com/example/apporderfood/adapter/FoodViewHolder.java
+++ app/src/main/java/com/example/apporderfood/adapter/FoodViewHolder.java
@@ -1,6 +1,7 @@
 package com.example.apporderfood.adapter;
 
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,6 +13,7 @@ public class FoodViewHolder extends RecyclerView.ViewHolder {
 
     ImageView imageView;
     TextView foodName, money, quantity;
+    ImageButton add, minus, btnRemove;
 
     public FoodViewHolder(View itemView) {
         super(itemView);
@@ -19,6 +21,9 @@ public class FoodViewHolder extends RecyclerView.ViewHolder {
         foodName = itemView.findViewById(R.id.foodNameCart);
         money = itemView.findViewById(R.id.foodMoneyCart);
         quantity = itemView.findViewById(R.id.quantityCart);
+        add = itemView.findViewById(R.id.add);
+        minus = itemView.findViewById(R.id.minus);
+        btnRemove = itemView.findViewById(R.id.btnRemove);
     }
 
 }
